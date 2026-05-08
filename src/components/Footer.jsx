78ddb2889e075e25d53424b5c6e930649d3a3d05
Thineth Shehara <sheharan@unifysportschat.com>
2026-05-08 18:53:21 +0530

    footer ready

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -41,9 +41,25 @@ const trustItems = [
 ];
 
 const footerLinks = {
-  Products: ['Label Printers', 'Label Rolls', 'Ink & Supplies', 'Special Labels'],
-  'Popular Printers': ['Epson C3500', 'Epson C6000', 'Epson C7500', 'Compare Models'],
-  'Support & Services': ['Expert Advice', 'Test Printer Loan', 'Sample Prints', 'Ink Calculator', 'Help Center'],
+  Products: [
+    { name: 'Label Printers', href: '/category/labelprinters' },
+    { name: 'Label Rolls', href: '/category/labels-en-tickets' },
+    { name: 'Ink & Supplies', href: '/category/inkt-cartridges-nl' },
+    { name: 'Special Labels', href: '/category/specials' },
+  ],
+  'Popular Printers': [
+    { name: 'ZX1600i', href: '/products/zx1600i?type=simple' },
+    { name: 'EZ6350i', href: '/products/ez6350i?type=simple' },
+    { name: 'HD830i', href: '/products/hd830i?type=simple' },
+    { name: 'Compare Models', href: '#' },
+  ],
+  'Support & Services': [
+    { name: 'Expert Advice', href: '/support/expert' },
+    { name: 'Test Printer Loan', href: '/support/loan' },
+    { name: 'Sample Prints', href: '/support/samples' },
+    { name: 'Ink Calculator', href: '/resources/ink-calculator' },
+    { name: 'Help Center', href: '/support' },
+  ],
 };
 
 export default function Footer() {
@@ -94,19 +110,6 @@ export default function Footer() {
                   BusinessLabels is onderdeel van Smart2B BV
                 </p>
               </div>
-              {/* Partner logos placeholder */}
-              {/* <div className="flex flex-col gap-2">
-                <div className="w-[276px] h-14 bg-white/10 rounded-md flex items-center justify-center">
-                  <span className="text-white/60 text-sm">Epson ColorWorks Partner</span>
-                </div>
-                <div className="w-[276px] h-14 bg-white/10 rounded-md flex items-center justify-center">
-                  <span className="text-white/60 text-sm">Diamondlabels</span>
-                </div>
-              </div>
-              <p className="text-white/80 text-base font-normal font-['Segoe_UI'] leading-5">
-                BusinessLabels is part of{' '}
-                <Link href="#" className="text-white/80 font-semibold underline">Smart2B BV</Link>
-              </p> */}
             </div>
             <div className="flex flex-col gap-4">
               <div className="flex items-end gap-2">
@@ -124,23 +127,25 @@ export default function Footer() {
                 </span>
               </div>
             </div>
-            <div>
-              <svg width="92" height="34" viewBox="0 0 92 34" fill="none" xmlns="http://www.w3.org/2000/svg"><g opacity="0.8"><rect width="92" height="34" rx="4" fill="#1E2939"/><g clipPath="url(#clip0_207_89)"><path fillRule="evenodd" clipRule="evenodd" d="M82.6239 8H66.4322C65.6578 8 65 8.58029 65 9.29514V24.704C65 25.4197 65.4316 26 66.206 26H82.3977C83.173 26 84 25.4197 84 24.704V9.29514C84 8.58029 83.3992 8 82.6239 8Z" fill="white"/><path fillRule="evenodd" clipRule="evenodd" d="M82.6239 8H66.4322C65.6578 8 65 8.58029 65 9.29514V24.704C65 25.4197 65.4316 26 66.206 26H82.3977C83.173 26 84 25.4197 84 24.704V9.29514C84 8.58029 83.3992 8 82.6239 8ZM72.2381 14.8571H74.7959V16.0923H74.8239C75.2139 15.4263 76.3656 14.75 77.7897 14.75C80.523 14.75 81.2857 16.1249 81.2857 18.6714V23.4286H78.5714V19.1403C78.5714 18.0003 78.091 17 76.9673 17C75.6029 17 74.9524 17.8751 74.9524 19.3117V23.4286H72.2381V14.8571ZM67.7143 23.4286H70.4286V14.8571H67.7143V23.4286ZM70.7679 11.8571C70.768 12.0682 70.7242 12.2771 70.639 12.4721C70.5539 12.6671 70.429 12.8443 70.2715 12.9936C70.1141 13.1429 69.9272 13.2613 69.7214 13.3421C69.5156 13.4229 69.2951 13.4645 69.0723 13.4646C68.8496 13.4646 68.629 13.4231 68.4232 13.3424C68.2174 13.2617 68.0304 13.1434 67.8728 12.9942C67.7153 12.845 67.5903 12.6679 67.5051 12.4729C67.4198 12.278 67.3759 12.069 67.3759 11.858C67.3759 11.4319 67.5545 11.0233 67.8724 10.722C68.1904 10.4206 68.6217 10.2513 69.0714 10.2512C69.5212 10.251 69.9526 10.4202 70.2707 10.7213C70.5888 11.0225 70.7677 11.4311 70.7679 11.8571Z" fill="#0275B9"/><path d="M62.2857 23.4289H59.7524V22.4081H59.7252C59.1616 23.0269 58.1826 23.5361 56.5233 23.5361C54.2614 23.5361 52.3134 21.9238 52.3134 19.1526C52.3134 16.5126 54.2333 14.7503 56.6083 14.7503C58.0786 14.7503 58.9915 15.2458 59.5443 15.8783H59.5714V10.5718H62.2857V23.4289ZM57.2905 16.6789C55.7389 16.6789 54.8667 17.7272 54.8667 19.1209C54.8667 20.5155 55.7389 21.6075 57.2905 21.6075C58.844 21.6075 59.7524 20.5438 59.7524 19.1209C59.7524 17.6586 58.844 16.6789 57.2905 16.6789ZM51.1219 22.0481C50.4813 22.8341 49.1621 23.5361 47.0323 23.5361C44.1914 23.5361 42.3819 21.7532 42.3819 18.9529C42.3819 16.4646 44.0204 14.7503 47.1119 14.7503C49.7819 14.7503 51.4286 16.4115 51.4286 19.3335C51.4286 19.6249 51.3788 19.9146 51.3788 19.9146H44.9423L44.9577 20.1795C45.1359 20.9183 45.7249 21.8218 47.0767 21.8218C48.2565 21.8218 49.0653 21.2209 49.4309 20.7503L51.1219 22.0481ZM48.8165 18.2852C48.8346 17.3818 48.1343 16.4646 47.0305 16.4646C45.7168 16.4646 45.0156 17.4366 44.9423 18.2861H48.8165V18.2852Z" fill="white"/><path d="M42.381 14.8574H39.2143L36.0476 18.2859V10.5717H33.3333V23.4288H36.0476V19.1431L39.3943 23.4288H42.599L38.7619 18.7428L42.381 14.8574ZM22.4762 14.8574H25.034V16.0925H25.062C25.452 15.4265 26.6037 14.7502 28.0278 14.7502C30.7611 14.7502 31.5238 16.3934 31.5238 18.6717V23.4288H28.8095V19.1405C28.8095 17.8788 28.3291 17.0002 27.2054 17.0002C25.841 17.0002 25.1905 18.0237 25.1905 19.3119V23.4288H22.4762V14.8574ZM17.9524 23.4288H20.6667V14.8574H17.9524V23.4288ZM19.3104 13.4002C19.5275 13.4051 19.7433 13.3688 19.9453 13.2934C20.1474 13.2181 20.3314 13.1053 20.4867 12.9615C20.642 12.8178 20.7655 12.6462 20.8497 12.4566C20.934 12.2671 20.9774 12.0635 20.9774 11.8578C20.9774 11.6521 20.934 11.4485 20.8497 11.259C20.7655 11.0694 20.642 10.8978 20.4867 10.7541C20.3314 10.6103 20.1474 10.4975 19.9453 10.4222C19.7433 10.3468 19.5275 10.3105 19.3104 10.3154C18.8853 10.3249 18.4809 10.4915 18.1837 10.7798C17.8866 11.068 17.7202 11.4549 17.7202 11.8578C17.7202 12.2607 17.8866 12.6476 18.1837 12.9358C18.4809 13.2241 18.8853 13.3907 19.3104 13.4002ZM10.7143 10.5717H8V23.4288H16.1429V20.8574H10.7143V10.5717Z" fill="white"/></g></g><defs><clipPath id="clip0_207_89"><rect width="76" height="18" fill="white" transform="translate(8 8)"/></clipPath></defs></svg>
-            </div>
           </div>
 
           {/* Link columns */}
           {Object.entries(footerLinks).map(([title, links]) => (
             <div key={title} className="w-56 flex flex-col gap-5">
-              <span className="text-white text-lg font-semibold font-['Segoe_UI'] leading-6">{title}</span>
+              <Link 
+                href={title === 'Products' ? '/products' : '#'} 
+                className={`text-white text-lg font-semibold font-['Segoe_UI'] leading-6 ${title === 'Products' ? 'hover:text-amber-500 transition-colors' : 'pointer-events-none'}`}
+              >
+                {title}
+              </Link>
               <div className="flex flex-col gap-4">
-                {links.map((link) => (
+                {links.map((item) => (
                   <Link
-                    key={link}
-                    href="#"
+                    key={item.name}
+                    href={item.href}
                     className="text-white/80 text-base font-normal font-['Segoe_UI'] leading-5 hover:text-white transition-colors"
                   >
-                    {link}
+                    {item.name}
                   </Link>
                 ))}
               </div>
