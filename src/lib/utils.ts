b8878699eba83159f313749c1d73ecd7908f0f66
Hirushan Perera <122775119+hirushann@users.noreply.github.com>
2026-05-08 15:11:48 +0530

    dynamic pages fix

---
+++ src/lib/utils.ts
@@ -0,0 +1,17 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function unescapeHtml(html: string) {
+  if (!html) return '';
+  return html
+    .replace(/&lt;/g, "<")
+    .replace(/&gt;/g, ">")
+    .replace(/&amp;/g, "&")
+    .replace(/&quot;/g, '"')
+    .replace(/&#39;/g, "'")
+    .replace(/&nbsp;/g, ' ');
+}
