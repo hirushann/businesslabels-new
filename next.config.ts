c26b31d311c8502fcd58d7bc1981e8df1ababf40
Hasith Udayanga <hasithudayanga@192.168.1.7>
2026-05-12 17:11:12 +0530

    fixes

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "placehold.co",
       },
+      {
+        protocol: "https",
+        hostname: "bbnl.dayzsolutions.com",
+      },
     ],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
