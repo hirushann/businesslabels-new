ad23b538203c04a3fde056b3f17710666a302383
Hasith Udayanga <hasithudayanga@192.168.1.6>
2026-05-16 08:49:07 +0530

    fixes

---
+++ src/app/group-products/[slug]/page.tsx
@@ -0,0 +1,34 @@
+import SingleProductPage, { generateMetadata as baseMetadata } from "../../products/[slug]/page";
+
+type PageProps = {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ type?: string | string[] }>;
+};
+
+export async function generateMetadata(props: PageProps) {
+  const params = await props.params;
+  const searchParams = await props.searchParams;
+  
+  // Force type to group_product for this route
+  const forcedSearchParams = { ...searchParams, type: "group_product" };
+
+  return baseMetadata({
+    params: Promise.resolve(params),
+    searchParams: Promise.resolve(forcedSearchParams),
+  });
+}
+
+export default async function GroupProductPage(props: PageProps) {
+  const params = await props.params;
+  const searchParams = await props.searchParams;
+
+  // Force type to group_product for this route
+  const forcedSearchParams = { ...searchParams, type: "group_product" };
+
+  return (
+    <SingleProductPage
+      params={Promise.resolve(params)}
+      searchParams={Promise.resolve(forcedSearchParams)}
+    />
+  );
+}
