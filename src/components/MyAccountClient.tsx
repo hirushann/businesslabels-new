39e4a228e99220e80576f674ed48134e38704897
Hirushan Perera <hirushanperera@Hirushans-MacBook-Air.local>
2026-07-03 13:39:57 +0530

    build error fix

--- src/components/MyAccountClient.tsx
+++ src/components/MyAccountClient.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { Fragment, useEffect, useState, Suspense, useSyncExternalStore, Fragment } from 'react';
+import React, { Fragment, useEffect, useState, Suspense, useSyncExternalStore } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { useSearchParams } from 'next/navigation';
